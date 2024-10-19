b5f4355458ed877483d22f1981f8822b92ee3d74
mohahmadi322 <mohammadahmadi3@cmail.carleton.ca>
2024-10-19 15:55:07 -0400

    added javadoc for players

--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,23 @@
 public class Player {
-    String name;
+    private String name;
+    private int score;
+
+    /**
+     * Cosntructor method for player
+     * initializes name, score, and tiles
+     * @param name
+     */
     public Player(String name){
         this.name = name;
+        score = 0;
+    }
+
+
+    /**
+     * Getter method for player score
+     * @return score
+     */
+    public int getScore(){
+        return score;
     }
 }
